d4e6ed2c647a3ef4887e3c6cf974ecdca4aa071a
Salakar <mike.diarmid@gmail.com>
2019-01-22 19:44:04 +0000

    add linting + prettier

--- index.js
+++ index.js
@@ -1,29 +1,31 @@
-function parseFields(fields) {
-    const res = {};
-    const keys = Object.keys(fields);
-    for (let i = 0, len = keys.length; i < len; i++) {
-        const key = keys[i];
-        res[key] = parseValue(fields[key]);
-    }
-    return res;
+function parseValue(value) {
+  const type = Object.keys(value)[0];
+
+  switch (type) {
+    case 'geoPointValue':
+      return [value.geoPointValue.latitude, value.geoPointValue.longitude];
+    case 'arrayValue':
+      return typeof value.arrayValue.values !== 'undefined'
+        ? value.arrayValue.values.map(parseValue)
+        : [];
+    case 'mapValue':
+      return parseFields(value.mapValue.fields);
+    case 'integerValue':
+    case 'doubleValue':
+      return Number(value[type]);
+    default:
+      return value[type];
+  }
 }
 
-function parseValue(value) {
-    const type = Object.keys(value)[0];
-
-    switch (type) {
-        case 'geoPointValue':
-            return [value.geoPointValue.latitude, value.geoPointValue.longitude];
-        case 'arrayValue':
-            return (typeof value.arrayValue.values !== 'undefined') ? value.arrayValue.values.map(parseValue) : [];
-        case 'mapValue':
-            return parseFields(value.mapValue.fields);
-        case 'integerValue':
-        case 'doubleValue':
-            return Number(value[type]);
-        default:
-            return value[type];
-    }
+function parseFields(fields) {
+  const res = {};
+  const keys = Object.keys(fields);
+  for (let i = 0, len = keys.length; i < len; i++) {
+    const key = keys[i];
+    res[key] = parseValue(fields[key]);
+  }
+  return res;
 }
 
 module.exports = parseFields;
